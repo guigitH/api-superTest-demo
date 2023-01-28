01acb3acfd64d1c8f33b05d0532863c67f82d8f4
Guillaume <slack77@hotmail.fr>
2023-01-28 18:18:47 +0100

    Add mocked test for demo

---
+++ testsMocked/postRequest.spec.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const expect = require('chai').expect;
+const app = require('./app');
+
+describe('post requests' , () => {
+    it('json response', () => {
+        request(app)
+        .post('/course')
+        .send({"name": "supertest"})
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+            expect(res.body.name).to.be.equal('supertest')
+        })
+    })
+
+    it('form response', (done) => {
+        request(app)
+        .post('/course')
+        .send('name=supertest')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .expect(200, {"id": "2", "name":"supertest"}, done)
+    })
+})
